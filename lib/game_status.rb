269039f2785578a3d5e6b381d75d0610b6538432
Sara Tibbetts <sxt4866@rit.edu>
2018-03-09 02:05:24 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,6 +8,6 @@ WIN_COMBINATIONS =  [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,
 
 def won?(board)
   WIN_COMBINATIONS.detect do |win_combo|
-    position_taken?(board[win_combo[0]]) && board[win_combo[0]] == board[win_combo[1]] && board[win_combo[1]] == board[win_]
+    position_taken?(board[win_combo[0]]) && board[win_combo[0]] == board[win_combo[1]] && board[win_combo[1]] == board[win_combo[2]]
   end 
 end 
